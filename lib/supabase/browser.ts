b79af30cf4d33e46f83729c8bf3829c8b1af3eea
zoooey99 <zoeyjleeweb@gmail.com>
2024-08-04 10:21:52 -0400

    authentication working, but does not redirect back to the home screen

--- lib/supabase/browser.ts
+++ lib/supabase/browser.ts
@@ -2,9 +2,15 @@
 
 import { createBrowserClient } from '@supabase/ssr'
 
-export function createClient() {
-  return createBrowserClient(
+// export function createClient() {
+//   return createBrowserClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//   )
+// }
+
+export const supabaseBrowser = () => 
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-}
+  );
